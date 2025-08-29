f8a79c01d3ed8e900463c6e7ed4bc1dbbf84ed9f
pluto-111 <prabinthakur0101@gmail.com>
2025-08-29 23:07:52 +0545

    removed to solve deployment conflicts

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  // Replace with your GitHub username and repository name
-  site: 'https://renovamen1.github.io',
-  base: '/renovamen1.github.io',
-});
