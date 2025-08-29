b40c58dad22555e67961a0a97a44a75ad0c65425
pluto-111 <prabinthakur0101@gmail.com>
2025-08-29 22:19:08 +0545

    added deploy.yml script for ci/cd

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // Replace with your GitHub username and repository name
+  site: 'https://renovamen1.github.io',
+  base: '/renovamen1.github.io',
+});
